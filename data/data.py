63df6c4d61ac2002c6debc6496cb0c48f66304b8
zainabmakhdum <90002363+zainabmakhdum@users.noreply.github.com>
2025-12-14 18:59:00 -0500

    Create data.py

---
+++ data/data.py
@@ -0,0 +1,8 @@
+print("delete this later")
+
+"""
+add the following sub-folders + content in this folder:
+  -> raw/ (add raw files for each company)
+  -> parsed/ (add parsed data here)
+  -> chunks/ (add chunks here)
+"""
